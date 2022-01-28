6ca2eb597e84bf51f9348d7dacd52d02961bb14a
yuchen203 <79148954+yuchen203@users.noreply.github.com>
2022-01-28 12:02:22 +0800

    complete ArsParser

---
+++ Cores/Utilities/Type.h
@@ -0,0 +1,10 @@
+#pragma once
+namespace PhysX {
+
+using	uchar = unsigned char;
+using	ushort = unsigned short;
+using	uint = unsigned int;
+using	llong = long long;
+using	ullong = unsigned long long;
+
+}
